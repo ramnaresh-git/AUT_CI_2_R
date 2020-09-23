0df7f1b965dd56e56b9de212365f900e63a535ec
ramnaresh-git <71754198+ramnaresh-git@users.noreply.github.com>
2020-09-23 07:37:40 -0700

    Update UnitTest1.cs

--- AUT_mstest1/UnitTest1.cs
+++ AUT_mstest1/UnitTest1.cs
@@ -2,12 +2,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AUT_mstest1
 {
-    [TestClass]
+     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(true);
+        }
+         [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.IsTrue(true);
         }
     }
 }
