4daa6f457516624d6b27aa9271117a926366cffa
ramnaresh-git <71754198+ramnaresh-git@users.noreply.github.com>
2020-09-23 06:46:42 -0700

    unit test

---
+++ AUT_mstest1/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AUT_mstest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
